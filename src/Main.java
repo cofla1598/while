a6a6c81cd0a439e00942e88dd9022a2b05c7b8e1
java-positivo-negativo <aguileraandres86@gmail.com>
2023-05-13 19:57:06 -0500

    se crea funcion incrementary mostrar mostarndo en pantalla cada vez que la variable aumente

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        incrementarYMostrar();
+
+
+    }
+    public static void incrementarYMostrar(){
+        int numerWhile=0;
+        while (numerWhile<3){
+            numerWhile++;
+            System.out.println(numerWhile);
+
+        }
+    }
+}
